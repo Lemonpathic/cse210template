2564b580e730faf6d6efa1f18e7dff933ea7ac40
Lemonpathic <124409140+Lemonpathic@users.noreply.github.com>
2024-01-10 14:00:02 -0700

    I did prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
+class Person{
+    private string _firstName;
+    private string _lastName;
+    public string firstName{
+        get {return _firstName; }
+        set {
+            if (!string.IsNullOrWhiteSpace(value)){
+                _firstName = value;
+            }
+            else{
+                System.Console.WriteLine("The name cannot be blank");
+            }
+            }
+    }
+     public string lastName{
+        get {return _lastName; }
+        set {
+            if (!string.IsNullOrWhiteSpace(value)){
+                _lastName = value;
+            }
+            else{
+                System.Console.WriteLine("The name cannot be blank");
+            }
+            }
+    }
+        public void doTheBondThing(){
+            System.Console.WriteLine();
+            Console.WriteLine($"Your name is {_lastName}, {_lastName} {_firstName}");
+        }
+    }
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Person person = new Person();
+
+        System.Console.WriteLine("What is your first name?");
+        person.firstName = Console.ReadLine();
+        System.Console.WriteLine("What is your last name?");
+        person.lastName = Console.ReadLine();
+
+        person.doTheBondThing();
+
     }
 }

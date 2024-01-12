f9c306b9dfb7f3ee1616e44836bf68b39c3418c7
Lemonpathic <124409140+Lemonpathic@users.noreply.github.com>
2024-01-12 12:34:32 -0700

    Did the magic number game!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+
+class NumberGame{
+    int Guesses;
+    int randomNumber;
+    public NumberGame(){
+        Random generatedNumber = new Random();
+        randomNumber = generatedNumber.Next(1,500);
+        MakeGuess();
+    }
+    private void MakeGuess(){
+        do{
+            System.Console.WriteLine("Make a guess of a number");
+            int guess = int.Parse(Console.ReadLine());
+            if(guess == randomNumber){
+                System.Console.WriteLine("You got it!");
+                break;
+            }
+            else if(guess > randomNumber){
+                System.Console.WriteLine("Lower!");
+            }
+            else if(guess < randomNumber){
+                System.Console.WriteLine("Higher!");
+            }
+        }while(true);
+
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string selection;
+        do{
+            System.Console.WriteLine("Welcome to number guesser!");
+            System.Console.WriteLine("[1] Play the game!");
+            System.Console.WriteLine("[2] Quit!");
+
+            selection = Console.ReadLine();
+            if(selection == "1"){
+                NumberGame game = new NumberGame();
+            }
+            else if(selection == "2"){
+                System.Console.WriteLine("Goodbye!");
+                break;
+            }
+            else{
+                System.Console.WriteLine("Invalid Option!");
+            }
+        }while(true);
     }
 }

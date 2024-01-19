ba75b0bc37c27f86bee42861443898b80192771d
Lemonpathic <124409140+Lemonpathic@users.noreply.github.com>
2024-01-19 10:27:22 -0700

    Finished this weeks assignments

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome buddy!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name?");
+            return Console.ReadLine();
+        }
+
+        static string PromptUserPhoneNumber()
+        {
+            Console.WriteLine("What is your phone number?");
+            return Console.ReadLine();
+        }
+
+        static int SquareNumber()
+        {
+            Console.WriteLine("Give me a number to square!"); 
+            int number = int.Parse(Console.ReadLine());
+            return number * number;
+        }
+
+        static void DisplayResult()
+        {
+            WelcomeMessage();
+            var name = PromptUserName();
+           var phoneNumber = PromptUserPhoneNumber();
+            var squared = SquareNumber();
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine(name);
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(squared);
+        }
+        DisplayResult();
     }
 }
